82c3e64515fa4f90d380ef420d07959306f7638c
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2024-09-08 23:29:13 -0500

    test

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    console.log("Game started");
+};
+
+const player ={
+    name : "Golf Guy",
+    wisdom : 10,
+    strength : 10,
+    dex : 12
+}
+
+const playerStats = document.getElementById('playerStats');
+const swingBtn = document.querySelector('.swingButton');
+const spellBtn = document.querySelector('.castSpellButton');
+
